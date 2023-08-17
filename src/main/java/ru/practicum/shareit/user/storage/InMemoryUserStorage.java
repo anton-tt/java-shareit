ffe4b34bf68d72def33cb6b6edd89f56f97a165a
anton-tt <carduelis@yandex.ru>
2023-08-17 12:33:43 +0700

    style: удалить неиспользуемый импорт

--- src/main/java/ru/practicum/shareit/user/storage/InMemoryUserStorage.java
+++ src/main/java/ru/practicum/shareit/user/storage/InMemoryUserStorage.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.user.storage;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
